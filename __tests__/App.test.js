a31080ef6f8ee459e6eb702a0041edb43e0e3ce0
maliza constantine <malizacoco4@gmail.com>
2020-11-17 17:43:41 +0200

    delete ddelete duplicate test directory

--- __tests__/App.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from '../App';
-
-it('renders welcome message', () => {
-    render(<App />);
-    expect(screen.getByText('Welcome to Barefoot Nomad')).toBeInTheDocument();
-});
