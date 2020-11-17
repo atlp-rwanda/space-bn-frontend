4aeaa5bc04192d98643cbd1b7108a4a8f66f0b4e
maliza constantine <malizacoco4@gmail.com>
2020-11-17 17:11:18 +0200

    change script test and create test directory

--- __tests__/App.test.js
+++ __tests__/App.test.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import Title from './Title';
+import App from '../App';
 
 it('renders welcome message', () => {
-    render(<Title />);
+    render(<App />);
     expect(screen.getByText('Welcome to Barefoot Nomad')).toBeInTheDocument();
 });
