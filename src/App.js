bd463911eeb9817c5ac7bfb68758f28541d6120b
SamuelSabasNayo <nshusamuel1@gmail.com>
2020-12-12 12:32:27 +0200

    ft: Added FAQ page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import Home from './views/Home';
-import Login from './views/Login/Login';
+import Login from './views/Login/Login'; 
 import Signup from './views/Signup';
 import Hotel from './views/Hotel';
+import FAQ from './views/FAQ';
 import './App.css';
 
 
@@ -16,6 +17,7 @@ function App() {
           <Route exact path="/hotel" component={Hotel}/>
           <Route exact path="/login" component={Login}/>
           <Route exact path="/signup" component={Signup}/>
+          <Route exact path="/faq" component={FAQ}/>
         </Switch>
       </div>
     </Router>
