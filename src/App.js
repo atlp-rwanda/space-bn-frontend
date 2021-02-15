d4a7118b8709c4cc9476c565c5da0ebd4f524450
Furebo Didace <52439710+furebo@users.noreply.github.com>
2021-02-15 14:41:28 +0200

    Ft user profile view #175743406 (#29)
    * rebased from develop
    
    * Implemetation of user profile view and edit
    
    * Implenetation of user profile view and edit
    
    * Implementation of user profile view and edit
    
    * Implementation of user profile view and edit
    
    * added the protection to the profile page
    
    * Integrated the user login and profile info edition with backend
    
    * added the image url field
    
    * Integrated login with backend
    
    * integration of login and setting the envirnoment
    
    * removes the commented lines
    
    * replaced localhost path by path from dotenv
    
    * removes the commented line

--- src/App.js
+++ src/App.js
@@ -35,44 +35,41 @@ function App() {
   return (
     <SignupContextProvider>
       <NotificationProvider>
-        <QuestionContextProvider>
-          <Router>
-            <div className="App">
-              <Switch>
-                <Route exact path="/" component={Home}/>
-                <Route exact path="/hotel" component={Hotel}/>
-                <Route exact path="/login" component={Login}/>
-                <Route exact path="/signup" component={Signup}/>
-                <Route path="/sidebar" component={Sidebar} />
-                <Route exact path="/faq" component={FAQ}/>
-                <Route exact path="/about" component={About}/>
-                <Route exact path="/contact" component={Contact}/>
-                <Route exact path="/profileview" component={profileDetailsView}/>
-                <ProtectedRoute exact path="/profile" component={Profile} />
-                <ProtectedRoute exact path="/booking" component={Accommadation} />
-                <ProtectedRoute exact path="/:hotelId/rooms">
-                  <RequestProvider>
-                    <Rooms />
-                  </RequestProvider>
-                </ProtectedRoute>
-                <ProtectedRoute exact path="/:hotelId/rooms/:roomId">
-                  <RequestProvider>
-                    <RoomDetail />
-                  </RequestProvider>
-                </ProtectedRoute>
-                <ProtectedRoute exact path="/requests/:reqId">
-                <ProtectedRoute exact path="/request">
-                  <RequestProvider>
-                    <TravelRequest />
-                  </RequestProvider>
-                </ProtectedRoute>
-                <ProtectedRoute exact path="/facilities" component={FacilityContainer} /> 
-                <ProtectedRoute exact path="/dashboard" component={Dashboard} />
-                <ProtectedRoute path="/requests/thread" exact component={RequestThread} />
-              </Switch>
-            </div>
-          </Router>
-        </QuestionContextProvider>
+        <Router>
+          <div className="App">
+            <Switch>
+              <Route exact path="/" component={Home}/>
+              <Route exact path="/hotel" component={Hotel}/>
+              <Route exact path="/login" component={Login}/>
+              <Route exact path="/signup" component={Signup}/>
+              <Route path="/sidebar" component={Sidebar} />
+              <Route exact path="/faq" component={FAQ}/>
+              <Route exact path="/about" component={About}/>
+              <Route exact path="/contact" component={Contact}/>
+              <Route exact path="/profileview" component={profileDetailsView}/>
+              <ProtectedRoute exact path="/profile" component={Profile} />
+              <ProtectedRoute exact path="/booking" component={Accommadation} />
+              <ProtectedRoute exact path="/:hotelId/rooms">
+                <RequestProvider>
+                  <Rooms />
+                </RequestProvider>
+              </ProtectedRoute>
+              <ProtectedRoute exact path="/:hotelId/rooms/:roomId">
+                <RequestProvider>
+                  <RoomDetail />
+                </RequestProvider>
+              </ProtectedRoute>
+              <ProtectedRoute exact path="/requests/:reqId">
+                <RequestProvider>
+                  <TravelRequest />
+                </RequestProvider>
+              </ProtectedRoute>
+              <ProtectedRoute exact path="/facilities" component={FacilityContainer} /> 
+              <ProtectedRoute exact path="/dashboard" component={Dashboard} />
+              <ProtectedRoute path="/requests/thread" exact component={RequestThread} />
+            </Switch>
+          </div>
+        </Router>
       </NotificationProvider>
     </SignupContextProvider>
   );
